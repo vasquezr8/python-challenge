576f3caa11ed92822408fd5e34a6501575c1e774
vasquezr8 <vasquezr8@gmail.com>
2023-07-01 14:47:28 -0700

    First half of summary table done

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -17,20 +17,23 @@ with open("budget_data.csv") as csvfile:
 
         profit_losses = row[1]
 
-        total_profit_losses = total_profit_losses + profit_losses
+        total_profit_losses = total_profit_losses + int(profit_losses)
         total_months = total_months + 1
 
         if total_months > 1:
-            change = profit_losses - last_profit_losses
+            change = int(profit_losses) - int(last_profit_losses)
 
             sum_profit_losses = sum_profit_losses + change
 
         last_profit_losses = row[1]
 
     # Summary table outside of loop
+    average_change = sum_profit_losses / (total_months - 1)
+    rounded_avg = round(average_change, 2)
+
     print("Financial Analysis")
     print("----------------------------")
 
     print(f"Total Months: {total_months}")
     print(f"Total: ${total_profit_losses}")
-    print(f"Average Change: $({sum_profit_losses} / ({total_months} - 1))")
+    print(f"Average Change: ${rounded_avg}")
