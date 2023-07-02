21be4e7bfe870d547ffe4aecbbad695435229885
vasquezr8 <vasquezr8@gmail.com>
2023-07-02 14:32:00 -0700

    Vote counters all correct

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -18,9 +18,22 @@ with open("election_data.csv") as csvfile:
 
         total_votes_counter = total_votes_counter + 1
 
+        if candidate == "Charles Casper Stockham":
+            charles_votes_counter = charles_votes_counter + 1
+
+        elif candidate == "Diana DeGette":
+                diana_votes_counter = diana_votes_counter + 1
+
+        else:
+                raymon_votes_counter = raymon_votes_counter + 1
+
     # Summary table outside of loop
     print("Election Results")
     print("-------------------------")
 
     print(f"Total Votes: {total_votes_counter}")
     print("-------------------------")
+
+    print(f"{charles_votes_counter}")
+    print(f"{diana_votes_counter}")
+    print(f"{raymon_votes_counter}")
